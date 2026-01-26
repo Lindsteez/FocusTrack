73f08d29d6d5e19da6c85c2863c8b7a33bff6d02
exelo <exelo@example.com>
2026-01-26 15:10:17 +0100

    pushing select focus mode

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,36 +1,14 @@
-import { useState } from "react"
-import "./App.css"
-import NavbarMobile from "./components/NavbarMobile"
-
-const focusmodes = ["Deep work", "Meeting", "Break"]
+import "./App.css";
+import NavbarMobile from "./components/NavbarMobile";
+import FocusModeSelector from "./components/FocusModeSelector";
 
 function App() {
-  const [activeMode, setActiveMode] = useState("Deep work")
-
   return (
     <div className="App">
-      <NavbarMobile/>
-
-      <h2>Active mode: {activeMode}</h2>
-
-      <p>Focus modes:</p> 
-      <div>
-        {focusmodes.map((mode) => (
-         <button
-       key={mode}
-        onClick={() => setActiveMode(mode)}
-         style={{
-      fontWeight: activeMode === mode ? "bold" : "normal",
-      border: activeMode === mode ? "2px solid Black" : "1px solid Gray"
-    }}
-  >
-    {mode}
-  </button>
-))} 
-
-      </div>
+      <FocusModeSelector />
+      <NavbarMobile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

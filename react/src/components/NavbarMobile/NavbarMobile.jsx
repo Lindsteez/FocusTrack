651fbf75da5bd6e1580f02585b9db02ef1cf672e
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-02-02 15:24:01 +0100

    added ToDo functionality

--- react/src/components/NavbarMobile/NavbarMobile.jsx
+++ react/src/components/NavbarMobile/NavbarMobile.jsx
@@ -25,6 +25,31 @@ function NavbarMobile() {
         <span>History</span>
       </NavLink>
 
+      <NavLink to="/todo" className={linkClass}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          
+          <path d="M16 4h1a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1" />
+          <rect x="8" y="2" width="8" height="4" rx="1" />
+          
+          <path d="M8 11l1.5 1.5L12 10" />
+          <line x1="13" y1="11" x2="17" y2="11" />
+          <path d="M8 16l1.5 1.5L12 15" />
+          <line x1="13" y1="16" x2="17" y2="16" />
+        </svg>
+        <span>ToDo</span>
+      </NavLink>
+
+
       <NavLink to="/stats" className={linkClass}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <line x1="12" y1="20" x2="12" y2="10" />

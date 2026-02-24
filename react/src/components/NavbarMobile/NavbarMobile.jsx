4074814f6080260912ac5a38055cc1e083b2203e
oliviaaMach <olivia.mach@outlook.com>
2026-02-24 11:12:00 +0100

    mobile view lang, nav done

--- react/src/components/NavbarMobile/NavbarMobile.jsx
+++ react/src/components/NavbarMobile/NavbarMobile.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from 'react-router-dom'
 import styles from './NavbarMobile.module.css'
+import { useLanguage } from '../../pages/settings/languageContext/LanguageProvider'
 
 function NavbarMobile() {
+  const { t } = useLanguage();
   const linkClass = ({ isActive }) =>
     `${styles.navItem} ${isActive ? styles.active : ''}`
 
@@ -14,7 +16,7 @@ function NavbarMobile() {
           <line x1="12" y1="13" x2="15" y2="15" />
           <line x1="9" y1="2" x2="15" y2="2" />
         </svg>
-        <span>Dashboard</span>
+        <span>{t('nav.dashboard')}</span>
       </NavLink>
 
       <NavLink to="/history" className={linkClass}>
@@ -22,7 +24,7 @@ function NavbarMobile() {
           <path d="M9 11l3 3L22 4" />
           <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
         </svg>
-        <span>History</span>
+        <span>{t('nav.history')}</span>
       </NavLink>
 
       <NavLink to="/todo" className={linkClass}>
@@ -46,7 +48,7 @@ function NavbarMobile() {
           <path d="M8 16l1.5 1.5L12 15" />
           <line x1="13" y1="16" x2="17" y2="16" />
         </svg>
-        <span>ToDo</span>
+        <span>{t('nav.todo')}</span>
       </NavLink>
 
 
@@ -56,7 +58,7 @@ function NavbarMobile() {
           <line x1="18" y1="20" x2="18" y2="4" />
           <line x1="6" y1="20" x2="6" y2="14" />
         </svg>
-        <span>Stats</span>
+        <span>{t('nav.stats')}</span>
       </NavLink>
 
       <NavLink to="/settings" className={linkClass}>
@@ -74,7 +76,7 @@ function NavbarMobile() {
             <circle cx="12" cy="12" r="3" />
             <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51h.09a1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9c0 .66.39 1.26 1 1.51H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
           </svg>
-        <span>Settings</span>
+        <span>{t('nav.settings')}</span>
       </NavLink>
     </nav>
   )
